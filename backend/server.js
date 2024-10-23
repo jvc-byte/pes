749a660beea17510c3d57992f5a7d9b553a59d30
jvc-byte <jvc8463@gmail.com>
2024-10-23 16:56:36 +0100

    forgot to commit server.js

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,57 @@
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
-//Running on port
-const port = 8081
-
-// Establish Connection to database
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "coaching_site"
-});
+const port = 8081;
 
 const app = express();
-app.use(cors())
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "pes",
+});
+
+app.post("/dashboard", (req, res) => {
+  const updateData =
+    "UPDATE dht11 SET temperature = ?, humidity = ? WHERE id = ?";
+
+  console.log("Received data:", req.body);
+
+  const { temperature, humidity } = req.body;
+  db.query(updateData, [temperature, humidity, 1], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Error Updating data");
+    } else {
+      res.send("Data updated successfully");
+    }
+  });
+});
 
 app.get("/", (req, res) => {
-    return res.json("Hello there From Backend");
-})
+  console.log("Received GET request on root");
+  return res.json("Hello there From Backend");
+});
 
-app.get("/users", (req, res) => {
-    const getUsers = "SELECT * FROM user";
-    db.query(getUsers, (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.send(result);
-        }
-    })
-})
+app.get("/dashboard", (req, res) => {
+  const getData = "SELECT * FROM dht11";
+  db.query(getData, (err, data) => {
+    if (err) {
+      return res.json(err);
+    } else {
+      console.log(data);
+      return res.json(data);
+    }
+  });
+});
 
-app.listen(port, () => {
-    console.log("Server is running on port: " + port);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running on port: ${port}`);
 });
